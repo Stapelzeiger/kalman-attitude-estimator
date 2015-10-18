856414e6cd3f62696a7c8763933ac6f93ffa0ee9
Patrick Spieler <stapelzeiger@gmail.com>
2015-10-18 16:50:55 +0200

    define ekf_measure type

--- src/template_kalman.h
+++ src/template_kalman.h
@@ -7,7 +7,7 @@ template<typename scalar, int state_dim, int control_dim>
 void ekf_predict(Eigen::Matrix<scalar, state_dim, 1> &state,
                  Eigen::Matrix<scalar, state_dim, state_dim> &state_cov,
                  const Eigen::Matrix<scalar, control_dim, 1> &control,
-                 Eigen::Matrix<scalar, state_dim, state_dim> &process_noise_cov,
+                 const Eigen::Matrix<scalar, state_dim, state_dim> &process_noise_cov,
                  void (*state_prop_fn)(Eigen::Matrix<scalar, state_dim, 1> &state,
                                        const Eigen::Matrix<scalar, control_dim, 1> &control),
                  void (*state_prop_jacobian_fn)(const Eigen::Matrix<scalar, state_dim, 1> &state,
@@ -21,5 +21,19 @@ void ekf_predict(Eigen::Matrix<scalar, state_dim, 1> &state,
     state_cov = F * state_cov * F.transpose() + process_noise_cov;
 }
 
+
+template<typename scalar, int state_dim, int measurement_dim>
+void ekf_measure(Eigen::Matrix<scalar, state_dim, 1> &state,
+                 Eigen::Matrix<scalar, state_dim, state_dim> &state_cov,
+                 const Eigen::Matrix<scalar, measurement_dim, 1> &measurement,
+                 const Eigen::Matrix<scalar, measurement_dim, measurement_dim> &measurement_cov,
+                 void (*measurement_pred_fn)(const Eigen::Matrix<scalar, state_dim, 1> &state,
+                                             Eigen::Matrix<scalar, measurement_dim, 1> &out_pred),
+                 void (*measurement_pred_jacobian_fn)(const Eigen::Matrix<scalar, state_dim, 1> &state,
+                                                      Eigen::Matrix<scalar, measurement_dim, state_dim> &out_jacobian)
+                 )
+{
+    
+}
 
 #endif /* TEMPLATE_KALMAN_H */
